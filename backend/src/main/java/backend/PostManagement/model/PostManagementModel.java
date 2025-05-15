c77d70ed6f801b9d5f639e959af71bded496b038
SansithPerera <sansithrumal@gmail.com>
2025-05-16 01:12:43 +0530

    Update PostManagementModel.java

--- backend/src/main/java/backend/PostManagement/model/PostManagementModel.java
+++ backend/src/main/java/backend/PostManagement/model/PostManagementModel.java
@@ -27,3 +27,43 @@ public class PostManagementModel {
         this.description = description;
         this.media = media;
     }
+
+    public PostManagementModel() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getMedia() {
+        return media;
+    }
