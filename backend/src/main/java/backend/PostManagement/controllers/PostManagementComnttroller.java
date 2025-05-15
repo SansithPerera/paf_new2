e1235cf8864b717d13a7b41e92b5f763ead9963b
SansithPerera <sansithrumal@gmail.com>
2025-05-16 00:40:24 +0530

    postmanagemntcotroller.java

---
+++ backend/src/main/java/backend/PostManagement/controllers/PostManagementComnttroller.java
@@ -0,0 +1,8 @@
+package backend.PostManagement.controller;
+
+import backend.exception.ResourceNotFoundException;
+import backend.PostManagement.model.Comment;
+import backend.Notification.model.NotificationModel;
+import backend.PostManagement.model.PostManagementModel;
+import backend.Notification.repository.NotificationRepository;
+import backend.PostManagement.repository.PostManagementRepository
