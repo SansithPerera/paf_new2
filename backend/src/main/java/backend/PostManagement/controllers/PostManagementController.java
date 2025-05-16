8184a6d6f5a11f92e61814be300a8c7a5687c071
mandiraa25 <wtmandiradewmini@gmail.com>
2025-05-16 18:23:19 +0530

    Implement delete comment feature

--- backend/src/main/java/backend/PostManagement/controllers/PostManagementController.java
+++ backend/src/main/java/backend/PostManagement/controllers/PostManagementController.java
@@ -275,4 +275,23 @@ public class PostManagementController {
                     return ResponseEntity.ok(post);
                 })
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }       
+    }
+
+
+ 
+    @DeleteMapping("/{postId}/comment/{commentId}")
+    public ResponseEntity<PostManagementModel> deleteComment(
+            @PathVariable String postId,
+            @PathVariable String commentId,
+            @RequestParam String userID) {
+        return postRepository.findById(postId)
+                .map(post -> {
+                    post.getComments().removeIf(comment ->
+                            comment.getId().equals(commentId) &&
+                                    (comment.getUserID().equals(userID) || post.getUserID().equals(userID)));
+                    postRepository.save(post);
+                    return ResponseEntity.ok(post);
+                })
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+          
