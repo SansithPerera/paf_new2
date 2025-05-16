ea402a734d605806d49861f8be814e45bc70371a
mandiraa25 <wtmandiradewmini@gmail.com>
2025-05-17 01:27:46 +0530

    Update NotificationModel.java

--- backend/src/main/java/backend/Notifications/model/NotificationModel.java
+++ backend/src/main/java/backend/Notifications/model/NotificationModel.java
@@ -41,3 +41,7 @@ public class NotificationModel {
     public String getMessage() {
         return message;
     }
+
+        public void setMessage(String message) {
+        this.message = message;
+    }
