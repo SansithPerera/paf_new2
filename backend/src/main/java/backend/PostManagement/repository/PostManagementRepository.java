345c286184345bc920a7f03aae08c5261b8efadd
SansithPerera <sansithrumal@gmail.com>
2025-05-16 09:19:22 +0530

    Update PostManagementRepository.java

--- backend/src/main/java/backend/PostManagement/repository/PostManagementRepository.java
+++ backend/src/main/java/backend/PostManagement/repository/PostManagementRepository.java
@@ -2,3 +2,7 @@ package backend.PostManagement.repository;
 
 import backend.PostManagement.model.PostManagementModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PostManagementRepository extends MongoRepository<PostManagementModel, String> {
+    void deleteByUserID(String userID); // Ensure this method exists
+}
