34c96cdbb452fb33cc07f16cab997f22e8d835f8
danuki15 <danukifer15@gmail.com>
2025-05-17 00:50:42 +0530

    Defining Communications

--- backend/src/main/java/backend/CommunityGroups/model/CommunityGroupsModel.java
+++ backend/src/main/java/backend/CommunityGroups/model/CommunityGroupsModel.java
@@ -7,3 +7,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 import java.util.ArrayList;
 
+@Document(collection = "Communications")
+public class CommunicationsModel {
+    @Id
+    @GeneratedValue
+    private String id;
+    private String adminID;
+    private String adminName;
+    private String groupTitle;
+    private String groupDescription;
+    private List<String> groupMembersIDs = new ArrayList<>();
+
+    public CommunicationsModel() {
+
+    }
+
+    
