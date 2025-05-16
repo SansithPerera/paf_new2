05c80cf71b4434afa0491651e3857f88bfac31e9
mandiraa25 <wtmandiradewmini@gmail.com>
2025-05-16 22:49:58 +0530

    Add NotificationController

---
+++ backend/src/main/java/backend/Notifications/controller/NotificationController.java
@@ -0,0 +1,9 @@
+package backend.Notification.controller;
+
+import backend.Notification.model.NotificationModel;
+import backend.Notification.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
