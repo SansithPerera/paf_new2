f32beb169fad40793f62996b84ca45d65a7d6799
danuki15 <danukifer15@gmail.com>
2025-05-18 13:49:54 +0530

    Update UpdateGroupDetails.js

--- frontend/src/pages/CommunityGroups/UpdateGroupDetails.js
+++ frontend/src/pages/CommunityGroups/UpdateGroupDetails.js
@@ -22,3 +22,23 @@ function UpdateGroupDetails() {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:8080/communications/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        alert('Group updated successfully!');
+        window.location.href = '/myGroup';
+      } else {
+        const errorData = await response.json();
+        alert(errorData.message || 'An error occurred.');
+      }
+    } catch (error) {
+      console.error('Error updating group:', error);
+      alert(error.message || 'An error occurred.');
+    }
+  };
